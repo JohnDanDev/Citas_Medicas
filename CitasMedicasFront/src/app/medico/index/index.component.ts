db85c1ed563fe0a9520f2820172b90a52753a24f
Daniel <juand.gor@gmail.com>
2024-10-10 19:53:53 -0600

    Se agrego codigo al componente indexts

--- CitasMedicasFront/src/app/medico/index/index.component.ts
+++ CitasMedicasFront/src/app/medico/index/index.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MedicoService } from '../medico.service';
+import { Medico } from '../medico';
+
 
 @Component({
   selector: 'app-index',
-  standalone: true,
-  imports: [],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit{
+  medicos: Medico[] = [];
 
+  constructor(public medicoService: MedicoService){}
+
+  ngOnInit(): void {
+    this.medicoService.getAll().subscribe((data: Medico[])=>{
+      this.medicos = data;
+      console.log(this.medicos);
+    })
+  }
+
+  deleteMedico(id_medico: any){
+    this.medicoService.delete(id_medico).subscribe(res =>{
+      this.medicos = this.medicos.filter(item=>item.id_medico !== id_medico);
+      console.log('Medico deleted successfully!');
+    })
+  }
 }
